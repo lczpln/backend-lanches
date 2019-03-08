f6b0f2bbc1a5974b0e74ab164b4f0ce3830dc4a2
lczpln <37776923+lczpln@users.noreply.github.com>
2019-03-08 00:00:54 -0300

    port fixed!

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(require('./routes'));
 
-const port = 3000 || process.env.PORT;
+var port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log("Server online! (:")
